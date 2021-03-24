2e5f160ff3f27f591b5f43068896f257e2ec4662
OlehBabiak <oleg_bob@ukr.net>
2021-03-24 16:44:05 +0200

    HW1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Main/>
+        <Footer/>
       </div>
     );
   }
