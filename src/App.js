47d764df940f1158c2d21e75e76f5b730f61f04d
OlehBabiak <oleg_bob@ukr.net>
2021-03-27 20:15:58 +0200

    create hw2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 
+//azaza
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
-        <Main/>
+        <Main/>   
         <Footer/>
       </div>
     );
