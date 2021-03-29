3847ccd96b763d8ed4fc325c18785f829ad6c0ac
OlehBabiak <oleg_bob@ukr.net>
2021-03-29 22:02:37 +0300

    23

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,12 +1,19 @@
 import React, {useState, Component} from 'react'
 
+// export default function Items({props}) {
+//     return (
+//         <div>
+//             <li>
+//                 {props.id} - {props.content}
+//             </li>
+//         </div>
+//     )
+// }
+
 export default function Items({props}) {
     return (
-        <div>
-            <li>
-                {props.id} - {props.content}
-            </li>
+        <div className={'square'}>
+           <p>{'I`m visiable!'}</p>
         </div>
     )
 }
-
